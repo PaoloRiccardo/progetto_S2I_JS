436a1db5a9c7cb061fb504a531087a42fbd485f1
PaoloRiccardo <102251153+PaoloRiccardo@users.noreply.github.com>
2022-05-03 19:57:28 +0200

    Aggiunta nuove icone

--- assets/script.js
+++ assets/script.js
@@ -6,28 +6,42 @@ const reset = document.getElementById("reset");
 add.addEventListener("click", () => {
     count.innerHTML++;
     applyColor();
+    countNumber()
 });
 
 sub.addEventListener("click",() =>{
     count.innerHTML--;
     applyColor();
-
+    countNumber()
 });
 
 reset.addEventListener("click", () => {
     count.innerHTML = 0;
     applyColor();
+    countNumber()
 });
 
 function applyColor() {
     if(count.innerHTML > 0) {
         count.style.color = "green";
+
     } else if (count.innerHTML < 0) {
         count.style.color = "red"; 
     } else {
         count.style.color = "black";  
     }
 }
+
+function countNumber(){
+    if(count.innerHTML == -5) {
+        alert("Are you sure you want to continue? So you won't save them!")
+    } else if(count.innerHTML == 15) {
+        alert("Good job! You saved two koalas! Go up to 30 to save four!")
+    } else if (count.innerHTML == 30) {
+        alert("Marvelous! You're doing great!")
+    }
+}
+
 
 
 
